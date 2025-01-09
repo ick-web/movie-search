7f19840664a804e7fd6952216504fff598a86c5e
ickhwan YU <ickhwanyu@icks-MacBook-Pro.local>
2025-01-09 15:30:58 +0900

    검색기능 추가

--- src/main.js
+++ src/main.js
@@ -7,25 +7,29 @@ const options = {
 };
 
 const moviesContainer = document.getElementById('movies-container');
+const searchInput = document.querySelector('.search-bar input');
+let movies = [];
 
+// TMDB API에서 인기 영화 가져오기
 fetch('https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1', options)
     .then(res => res.json())
     .then(data => {
-        const movies = data.results; //영화 데이터 배열
+        movies = data.results; //영화 데이터 배열
         displayMovies(movies)
     })
     .catch(err => console.error(err));
 
-//title,poster_path,vote_average 넣기
-// 영화 카드 생성 및 DOM에 추가
-function displayMovies(movies) {
+//title,poster_path,vote_average 넣기 클릭시 overview(요약) 보여주기
+// 영화 카드 생성 및 DOM에 추가.
+function displayMovies(moviesToDisplay) {
     moviesContainer.innerHTML = ''; // 기존 내용 초기화
-    movies.forEach(movie => {
-        const { title, poster_path, vote_average } = movie;
+    moviesToDisplay.forEach(movie => {
+        const { title, poster_path, vote_average, id } = movie;
 
         // 영화 카드 생성
         const movieCard = document.createElement('div');
         movieCard.classList.add('movie-card');
+        movieCard.setAttribute('data-id', id)
 
         // 포스터 이미지
         const poster = document.createElement('img');
@@ -49,6 +53,15 @@ function displayMovies(movies) {
         moviesContainer.appendChild(movieCard);
     });
 }
+
+// 검색 기능.
+searchInput.addEventListener('input', (e)=> {
+    const searchQuery = e.target.value.toLowerCase(); //검색어 소문자
+    const filteredMovies = movies.filter(movie => {
+        return movie.title.toLowerCase().includes(searchQuery); //대소문자 구분 없이 검색
+    })
+    displayMovies(filteredMovies); //필터링된 영화 리스트 표시
+});
 
 
 
