b108dc1ea58cd1cea939ab7b13264852ff7d3356
ickhwan YU <ickhwanyu@icks-MacBook-Pro.local>
2025-01-10 20:49:27 +0900

    카드 모달 정리

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ fetch('https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1', option
         movies = data.results; //영화 데이터 배열
         displayMovies(movies)
     })
+    
     .catch(err => console.error(err));
 
 // 영화 카드 생성 및 DOM에 추가.
@@ -24,7 +25,7 @@ function displayMovies(moviesToDisplay) {
     moviesContainer.innerHTML = ''; // 기존 내용 초기화 (새로운 영화 목록 표시)
     moviesToDisplay.forEach(movie => {
         const { title, poster_path, vote_average, id } = movie;
-
+        
         // 영화 카드 생성
         const movieCard = document.createElement('div');
         movieCard.classList.add('movie-card');
@@ -48,6 +49,9 @@ function displayMovies(moviesToDisplay) {
         movieCard.appendChild(movieTitle);
         movieCard.appendChild(rating);
 
+        // 모달 클릭 이벤트 추가..
+        movieCard.addEventListener('click', () => showMovieDetails(movie));
+
         // 컨테이너에 카드 추가
         moviesContainer.appendChild(movieCard);
     });
@@ -61,6 +65,50 @@ searchInput.addEventListener('input', (e)=> {
     })
     displayMovies(filteredMovies); //필터링된 영화 리스트 표시
 });
+
+// 모달창에 상세정보 표시
+function showMovieDetails(movie){
+    const modal = document.createElement('div');
+    modal.classList.add('modal')
+
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content')
+
+    const title = document.createElement('h2')
+    title.textContent = movie.title
+
+    const poster = document.createElement('img')
+    poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    poster.alt = `${movie.title} 포스터`;
+
+    // 요약
+    const overView = document.createElement('p');
+    overView.textContent = movie.overview;
+
+    // 평점
+    const rating = document.createElement('p');
+    rating.textContent = `평점: ${movie.vote_average}`;
+
+    //닫기 버튼
+    const closeButton = document.createElement('button')
+    closeButton.textContent = '닫기';
+    closeButton.addEventListener('click', () => {
+        document.body.removeChild(modal)
+
+    
+
+    });
+
+    modalContent.appendChild(title);
+    modalContent.appendChild(poster);
+    modalContent.appendChild(overView);
+    modalContent.appendChild(rating);
+    modalContent.appendChild(closeButton);
+    modal.appendChild(modalContent);
+
+    document.body.appendChild(modal);
+}
+
 
 
 
